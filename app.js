a994a178c0770780a1700f22a57c6ce291beb550
Bhavesh <bhaveshjat9950@gmail.com>
2021-02-03 17:53:23 +0530

    1

---
+++ app.js
@@ -0,0 +1,27 @@
+window.addEventListener("load", () => {
+	let long;
+	let lat;
+
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition((position) => {
+			console.log(position);
+
+			long = position.coords.longitude;
+			lat = position.coords.latitude;
+			console.log(long, lat);
+
+			// const proxy = "https://cors-anywhere.herokuapp.com/";
+
+			const api = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=aae516d8d0d0689378583bab648a1981`;
+			console.log(api);
+
+			fetch(api)
+				.then((response) => {
+					return response.json();
+				})
+				.then((data) => {
+					console.log(data);
+				});
+		});
+	}
+});
